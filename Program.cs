d8d36566d639b024f5c4fdb9f292727c6cc36132
Shit-Kid <39274387+Shit-Kid@users.noreply.github.com>
2018-05-14 19:46:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //@"‪D:\Images\JPEG\Skype.jpg"
+            System.Diagnostics.Process.Start(@"A:\Opera\launcher.exe" , "www.youtube.co.il");
+        }
+    }
+}
